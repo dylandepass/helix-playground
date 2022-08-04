66b792f84fb04d093138470638c741b3d6b724fa
dyland <djdyland@gmail.com>
2022-08-04 10:18:19 -0400

    fix: loading delay

--- blocks/3d/3d.js
+++ blocks/3d/3d.js
@@ -4,11 +4,15 @@
  */
 
 export default async function decorate(block) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const delay = urlParams.get('delay') || 0;
   const heading = document.querySelector('main > div div:nth-of-type(1)');
   heading.classList.add('hero');
   document.querySelector('header').remove();
-  const iframe = document.createElement('iframe');
-  iframe.loading = 'lazy';
-  iframe.src = 'https://my.spline.design/expressdevice-78262f5c36e6d392978f3b000d70456e/';
-  block.append(iframe);
+  setTimeout(() => {
+    const iframe = document.createElement('iframe');
+    iframe.loading = 'lazy';
+    iframe.src = 'https://my.spline.design/expressdevice-78262f5c36e6d392978f3b000d70456e/';
+    block.append(iframe);
+  }, delay);
 }
