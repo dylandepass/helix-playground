656e6d14b7ce2d7f25260e76db384e80c4ef3ef4
dyland <djdyland@gmail.com>
2022-08-03 23:57:13 -0400

    fix: more 3d demo tweaks

--- blocks/3d/3d.js
+++ blocks/3d/3d.js
@@ -4,6 +4,10 @@
  */
 
 export default async function decorate(block) {
+
+  const heading = document.querySelector('main > div div:nth-of-type(1)');
+  heading.classList.add('hero');
+
   const iframe = document.createElement('iframe');
   iframe.src = 'https://my.spline.design/expressdevice-78262f5c36e6d392978f3b000d70456e/';
   block.append(iframe);
