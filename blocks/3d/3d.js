e3596c8cbe92f5cca9c7d709ea6f686af233eb81
dyland <djdyland@gmail.com>
2022-08-04 10:09:43 -0400

    fix: load iframe lazy

--- blocks/3d/3d.js
+++ blocks/3d/3d.js
@@ -4,13 +4,11 @@
  */
 
 export default async function decorate(block) {
-
   const heading = document.querySelector('main > div div:nth-of-type(1)');
   heading.classList.add('hero');
-
+  document.querySelector('header').remove();
   const iframe = document.createElement('iframe');
+  iframe.loading = 'lazy';
   iframe.src = 'https://my.spline.design/expressdevice-78262f5c36e6d392978f3b000d70456e/';
   block.append(iframe);
-
-  document.querySelector('header').remove();
 }
