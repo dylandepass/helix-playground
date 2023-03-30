f5359a04dd3f2fe570dff242c15231fad05662fd
Dylan Depass <dylandepass@gmail.com>
2023-03-29 23:04:22 -0400

    feat: add block library

---
+++ blocks/library-config/library-config.js
@@ -0,0 +1,6 @@
+import franklinLibrary from 'https://main--helix-playground--dylandepass.hlx.page/library/library.js';
+
+export default function decorate(block) {
+  const library = document.createElement('franklin-library');
+  block.append(library);
+}
