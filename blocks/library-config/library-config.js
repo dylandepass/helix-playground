da2a11e3b958cff196498a8524357603fbe1d1ad
Dylan Depass <dylandepass@gmail.com>
2023-04-01 12:42:26 -0400

    fix: update library config

--- blocks/library-config/library-config.js
+++ blocks/library-config/library-config.js
@@ -2,5 +2,9 @@ import 'https://main--helix-playground--dylandepass.hlx.page/library/library.js'
 
 export default function decorate(block) {
   const library = document.createElement('franklin-library');
+  library.config = {
+    library: 'https://main--rosalind-boilerplate--dylandepass.hlx.page/storybook/library.json',
+    extends: 'https://main--wesco-ecommerce--wesco-international.hlx.page/storybook/library.json'
+  }
   block.append(library);
 }
