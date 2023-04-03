99a36db21089471aff7d82b7fb7179d9a7d7776c
Dylan Depass <dylandepass@gmail.com>
2023-04-03 17:07:18 -0400

    fix: taxonomy plugin from rosalind

--- blocks/library-config/library-config.js
+++ blocks/library-config/library-config.js
@@ -4,7 +4,8 @@ export default function decorate(block) {
   const library = document.createElement('franklin-library');
   library.config = {
     library: 'https://main--rosalind-boilerplate--dylandepass.hlx.page/storybook/library.json',
-    extends: 'https://main--wesco-ecommerce--wesco-international.hlx.page/storybook/library.json'
+    extends: 'https://main--wesco-ecommerce--wesco-international.hlx.page/storybook/library.json',
+    taxonomy: 'https://plugin-test--rosalind-boilerplate--dylandepass.hlx.page/blocks/taxonomy/taxonomy.js'
   }
   block.append(library);
 }
