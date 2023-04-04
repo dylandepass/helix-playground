3b20d860e732b401623d2e4bd6e1c0665821286f
Dylan Depass <dylandepass@gmail.com>
2023-04-04 10:51:00 -0400

    fix: pull config from blockConfig

--- blocks/library-config/library-config.js
+++ blocks/library-config/library-config.js
@@ -1,11 +1,16 @@
 import 'https://main--helix-playground--dylandepass.hlx.page/library/library.js';
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
+  const blockConfig = readBlockConfig(block);
+  console.log(blockConfig);
+  block.innerHTML = '';
   const library = document.createElement('franklin-library');
-  library.config = {
-    library: 'https://main--rosalind-boilerplate--dylandepass.hlx.page/storybook/library.json',
-    extends: 'https://main--wesco-ecommerce--wesco-international.hlx.page/storybook/library.json',
-    taxonomy: 'https://plugin-test--rosalind-boilerplate--dylandepass.hlx.page/blocks/taxonomy/taxonomy.js'
-  }
+  library.config = {};
+
+  Object.keys(blockConfig).forEach((attr) => {
+    library.config[attr] = blockConfig[attr];
+  })
+
   block.append(library);
 }
